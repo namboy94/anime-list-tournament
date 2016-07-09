95f5948a0ee905948490000166f871eed5a4ca5d
Hermann Krumrey <hermann@krumreyh.com>
2016-07-09 22:24:24 +0200

    Tried some stuff with HttpUrlConnection

--- lib/src/main/java/net/namibsun/maltourn/lib/Main.java
+++ lib/src/main/java/net/namibsun/maltourn/lib/Main.java
@@ -1,7 +1,38 @@
 package net.namibsun.maltourn.lib;
 
+import java.io.*;
+import java.net.*;
+
 /**
  * Created by hermann on 7/9/16.
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            System.out.print(getHTML("http://myanimelist.net"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static String getHTML(String websiteUrl) throws Exception {
+        //StringBuilder result = new StringBuilder();
+
+        String content = "";
+        URL url = new URL(websiteUrl);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            content += line + "\n";
+        }
+        reader.close();
+        return content;
+    }
+
 }
