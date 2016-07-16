cf6b699db2dba105f46c3353ee34aa3ddefaf8af
Hermann Krumrey <hermann@krumreyh.com>
2016-07-17 00:42:35 +0200

    Implemented image loading from URL

--- lib/src/main/java/net/namibsun/maltourn/lib/gui/SimpleVsRaterGui.java
+++ lib/src/main/java/net/namibsun/maltourn/lib/gui/SimpleVsRaterGui.java
@@ -2,8 +2,13 @@ package net.namibsun.maltourn.lib.gui;
 
 import net.namibsun.maltourn.lib.objects.AnimeSeries;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SimpleVsRaterGui extends JFrame {
 
@@ -32,18 +37,37 @@ public class SimpleVsRaterGui extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
 
-        this.setLayoutElements();
+        try {
+            this.setLayoutElements();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         this.setResizable(false);
         this.setVisible(true);
     }
 
-    private void setLayoutElements() {
+    private void setLayoutElements() throws IOException {
+
+        int labelWidth = 200;
+        int labelHeight = 50;
 
         this.leftContestantLabel = new JLabel("Test", SwingConstants.CENTER);
-        this.leftContestantLabel.setLocation(1, 1);
-        this.leftContestantLabel.setSize(200, 200);
+        this.leftContestantLabel.setLocation(10, 200);
+        this.leftContestantLabel.setSize(labelWidth, labelHeight);
         this.add(this.leftContestantLabel);
+
+        this.rightContestantLabel = new JLabel("Test", SwingConstants.CENTER);
+        this.rightContestantLabel.setLocation(250, 200);
+        this.rightContestantLabel.setSize(labelWidth, labelWidth);
+        this.add(this.rightContestantLabel);
+
+        URL leftImageUrl = new URL("https://pixabay.com/static/uploads/photo/2015/10/01/21/39/background-image-967820_960_720.jpg");
+        BufferedImage leftImage = ImageIO.read(leftImageUrl);
+        this.leftContestantImage = new ImageLabel(leftImage, 200, 200, ImageLabel.ScaleMode.STRETCH, Color.black);
+        this.leftContestantImage.setLocation(300, 300);
+        this.add(this.leftContestantImage);
+
 
     }
 
