ffd5f959138bb86880b7686aba6df862636c023e
Hermann Krumrey <hermann@krumreyh.com>
2016-07-17 01:09:42 +0200

    Rudimentary login system

--- lib/src/main/java/net/namibsun/maltourn/lib/gui/SimpleVsRaterGui.java
+++ lib/src/main/java/net/namibsun/maltourn/lib/gui/SimpleVsRaterGui.java
@@ -1,6 +1,9 @@
 package net.namibsun.maltourn.lib.gui;
 
+import net.namibsun.maltourn.lib.gets.Authenticator;
+import net.namibsun.maltourn.lib.gets.ListGetter;
 import net.namibsun.maltourn.lib.objects.AnimeSeries;
+import net.namibsun.maltourn.lib.posts.ScoreSetter;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -8,6 +11,9 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
 
 public class SimpleVsRaterGui extends JFrame {
 
@@ -18,6 +24,8 @@ public class SimpleVsRaterGui extends JFrame {
     JLabel rightContestantImage;
     AnimeSeries leftContestant;
     AnimeSeries rightContestant;
+    ScoreSetter scoreSetter = null;
+    ArrayList<AnimeSeries> series = new ArrayList<>();
 
 
     public SimpleVsRaterGui() {
@@ -36,6 +44,8 @@ public class SimpleVsRaterGui extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
 
+        this.login();
+
         try {
             this.setLayoutElements();
         } catch (IOException e) {
@@ -46,6 +56,27 @@ public class SimpleVsRaterGui extends JFrame {
         this.setVisible(true);
     }
 
+    private void login() {
+        while (this.scoreSetter == null) {
+            String username = JOptionPane.showInputDialog(null, "Username");
+            String password = JOptionPane.showInputDialog(null, "Password");
+
+            if (!Authenticator.isAuthenticated(username, password)) {
+                JOptionPane.showConfirmDialog(null, "Invalid Username/Password");
+            }
+            else {
+                this.scoreSetter = new ScoreSetter(username, password);
+                Set<AnimeSeries> series = ListGetter.getList(username);
+                for (AnimeSeries anime: series) {
+                    this.series.add(anime);
+                }
+                Collections.shuffle(this.series);
+                this.leftContestant = this.series.remove(0);
+                this.rightContestant = this.series.remove(0);
+            }
+        }
+    }
+
     private void setLayoutElements() throws IOException {
 
         int labelWidth = 100;
@@ -53,28 +84,27 @@ public class SimpleVsRaterGui extends JFrame {
         int imageWidth = 200;
         int imageHeight = 300;
 
-        this.leftContestantLabel = new JLabel("Left", SwingConstants.CENTER);
-        this.leftContestantLabel.setLocation(0, 0);
+        this.leftContestantLabel = new JLabel(this.leftContestant.seriesTitle, SwingConstants.CENTER);
+        this.leftContestantLabel.setLocation(150, 0);
         this.leftContestantLabel.setSize(labelWidth, labelHeight);
         this.add(this.leftContestantLabel);
 
-        this.rightContestantLabel = new JLabel("Right", SwingConstants.CENTER);
-        this.rightContestantLabel.setLocation(400, 0);
-        this.rightContestantLabel.setSize(labelWidth, labelWidth);
+        this.rightContestantLabel = new JLabel(this.rightContestant.seriesTitle, SwingConstants.CENTER);
+        this.rightContestantLabel.setLocation(550, 0);
+        this.rightContestantLabel.setSize(labelWidth, labelHeight);
         this.add(this.rightContestantLabel);
 
-        URL leftImageUrl = new URL("http://images.ichkoche.at/data/image/variations/496x384/1/vanillekipferl-das-klassische-rezept-img-327.jpg");
+        URL leftImageUrl = new URL(this.leftContestant.seriesImage);
         BufferedImage leftImage = ImageIO.read(leftImageUrl);
         this.leftContestantImage = new ImageLabel(leftImage, imageWidth, imageHeight, ImageLabel.ScaleMode.STRETCH, Color.black);
-        this.leftContestantImage.setLocation(0, 100);
+        this.leftContestantImage.setLocation(100, 100);
         this.add(this.leftContestantImage);
 
-        URL rightImageUrl = new URL("http://images.ichkoche.at/data/image/variations/496x384/4/vanillekipferl-rezept-img-31386.jpg");
+        URL rightImageUrl = new URL(this.rightContestant.seriesImage);
         BufferedImage rightImage = ImageIO.read(rightImageUrl);
         this.rightContestantImage = new ImageLabel(rightImage, imageWidth, imageHeight, ImageLabel.ScaleMode.STRETCH, Color.black);
-        this.rightContestantImage.setLocation(400, 100);
+        this.rightContestantImage.setLocation(500, 100);
         this.add(this.rightContestantImage);
-
 
     }
 
