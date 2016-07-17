8985a40055dcd5e20436ebba25a4b7687d51a0d9
Hermann Krumrey <hermann@krumreyh.com>
2016-07-17 22:46:47 +0200

    Added basic android metadata content

--- mal-tournament-android/src/main/java/net/namibsun/maltourn/android/MainActivity.java
+++ mal-tournament-android/src/main/java/net/namibsun/maltourn/android/MainActivity.java
@@ -1,7 +1,9 @@
 package net.namibsun.maltourn.android;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by hermann on 7/17/16.
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
 }
