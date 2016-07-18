084ac591f047f152ff67c6755e9c90a5fafc2058
Hermann Krumrey <hermann@krumreyh.com>
2016-07-18 17:58:51 +0200

    Implemented working login screen

--- mal-tournament-android/src/main/java/net/namibsun/maltourn/android/activities/LoginActivity.java
+++ mal-tournament-android/src/main/java/net/namibsun/maltourn/android/activities/LoginActivity.java
@@ -25,24 +25,24 @@ package net.namibsun.maltourn.android.activities;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Process;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import net.iharder.Base64;
 import net.namibsun.maltourn.android.R;
 import net.namibsun.maltourn.lib.gets.Authenticator;
-import net.namibsun.maltourn.lib.gets.ListGetter;
-import net.namibsun.maltourn.lib.objects.AnimeSeries;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+
 
 /**
  * Activity that handles the login of the user to myanimelist.net
@@ -51,7 +51,6 @@ public class LoginActivity extends AnalyticsActivity {
 
     private String username;
     private String password;
-    private ProgressBar loginProgressCircle;
 
     /**
      * Creates the login activity
@@ -59,35 +58,19 @@ public class LoginActivity extends AnalyticsActivity {
      */
     protected void onCreate(Bundle savedInstanceState) {
 
-        //Remove this!
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
         // this.analyticsActive = false;
         this.layoutFile = R.layout.activity_login;
         this.screenName = "Login";
         this.analyticsName = "MAL-Login";
         super.onCreate(savedInstanceState);
 
-        this.loginProgressCircle = (ProgressBar) this.findViewById(R.id.login_loader);
-        //this.loginProgressCircle.setVisibility(View.INVISIBLE);
         Button loginButton = (Button) this.findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LoginActivity.this.getLoginData();
-                // Network on main thread exceptions are bad!
-
-                if (Authenticator.isAuthenticated(LoginActivity.this.username, LoginActivity.this.password)) {
-                    LoginActivity.this.loginProgressCircle.setVisibility(View.VISIBLE);
-                    for (AnimeSeries serie: ListGetter.getList(LoginActivity.this.username)) {
-                        Log.e("Anime", serie.seriesTitle);
-                    }
-                    LoginActivity.this.loginProgressCircle.setVisibility(View.INVISIBLE);
-                } else {
-                    LoginActivity.this.showAuthenticationErrorDialog();
-                }
+                new AsyncLogin().execute();
             }
         });
     }
@@ -113,5 +96,31 @@ public class LoginActivity extends AnalyticsActivity {
         });
         errorDialogBuilder.create();
         errorDialogBuilder.show();
+    }
+
+    private void startOverViewActivity() {
+        Intent overViewActivity = new Intent(this, OverViewActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("username", this.username);
+        bundle.putString("password", this.password);
+        overViewActivity.putExtras(bundle);
+        this.startActivity(overViewActivity);
+
+    }
+
+    private class AsyncLogin extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... params) {
+            if (Authenticator.isAuthenticated(LoginActivity.this.username, LoginActivity.this.password)) {
+                LoginActivity.this.startOverViewActivity();
+            } else {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        LoginActivity.this.showAuthenticationErrorDialog();
+                    }
+                });
+            }
+            return null;
+        }
     }
 }
