2885c21761d27a4e3914c7e7bea858d81cafffe7
Hermann Krumrey <hermann@krumreyh.com>
2016-07-18 11:59:30 +0200

    Decided to create seperate lib module that's compatible to java 7

--- mal-tournament-android/src/main/java/net/namibsun/maltourn/android/MainActivity.java
+++ mal-tournament-android/src/main/java/net/namibsun/maltourn/android/MainActivity.java
@@ -1,9 +1,11 @@
 package net.namibsun.maltourn.android;
 
 import android.support.v7.app.AppCompatActivity;
+import net.namibsun.maltourn.java.cli.CliMalTournament;
 
 /**
  * Created by hermann on 7/17/16.
  */
 public class MainActivity extends AppCompatActivity {
+
 }
