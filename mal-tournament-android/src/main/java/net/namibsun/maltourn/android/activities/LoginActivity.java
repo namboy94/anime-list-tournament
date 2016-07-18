ed54709982f45eda1c4416e9a9fe4157b0db0eb3
Hermann Krumrey <hermann@krumreyh.com>
2016-07-18 14:08:07 +0200

    Created activities

---
+++ mal-tournament-android/src/main/java/net/namibsun/maltourn/android/activities/LoginActivity.java
@@ -0,0 +1,9 @@
+package net.namibsun.maltourn.android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hermann on 7/18/16.
+ */
+public class LoginActivity extends AppCompatActivity {
+}
